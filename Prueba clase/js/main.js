c378462cf915f6501abcc5326e1a80697eb83036
lucasbernalb <lucasbernalb@gmail.com>
2024-06-04 21:47:05 -0300

    1er commit

---
+++ Prueba clase/js/main.js
@@ -0,0 +1,28 @@
+// Esto es un comentario
+//alert("Hola Mundo!"); //Salida con el mensaje Hola Mundo!
+
+// Variables
+/* let mensaje = "Estamos en el Curso de JavaScript!"; //Definir una variable mensaje, con un texto [DATO]
+alert(mensaje); // Mostrar un alerta con el contenido de la variable "mensaje" [SALIDA]
+console.log("holis, que hacías?"); // [SALIDA]
+console.log(mensaje);
+
+function validarEdad() {
+    let edad = document.getElementById("edad").value;
+    console.log("Tu edad es: " + edad);
+    alert("Tu edad es: " + edad)
+} */
+
+// Modifico el contenido de mi encabezado con JS
+let encabezado = document.getElementById("curso");
+encabezado.innerHTML = "Curso de JavaScript...";
+encabezado.className = "text-white bg-black";
+
+let nombreUs = promt("Ingresa tu nombre");
+alert
+if(nombreUs == "") {
+    alert("No ingresaste nombre");
+}
+else{
+    alert("Nombre de usuario ingresado " + nombreUs);
+}
